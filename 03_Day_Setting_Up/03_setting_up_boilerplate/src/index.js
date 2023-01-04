e80765ff6f5e6a3c6db36029fd26d00b671da3a1
ouyang <ouyangliang@citorytech.com>
2023-01-04 16:45:43 +0800

    submit day03

--- 03_Day_Setting_Up/03_setting_up_boilerplate/src/index.js
+++ 03_Day_Setting_Up/03_setting_up_boilerplate/src/index.js
@@ -1,116 +1,133 @@
-// index.js
 import React from 'react'
 import ReactDOM from 'react-dom'
-// To get the root element from the HTML document
-import asabenehImage from './images/asabeneh.jpg'
-
-// to import the doSomeMath from the math.js with or without extension
-import doSomeMath from './math.js'
-
-// to import the other modules
-// since these modules were not exported as default we have to desctructure
-import { addTwo, multiply, subtract } from './math.js'
-
-import * as everything from './math.js'
-console.log(addTwo(5, 5))
-console.log(doSomeMath.addTwo(5, 5))
-console.log(everything)
-// JSX element, header
-
-
-// JSX element, header
+import cssImg from './img/css_logo.png'
+import htmlImg from './img/html_logo.png'
+import reactImg from './img/react_logo.png'
+import useImg from './img/img.jpg'
+// jsx header data
 const welcome = 'Welcome to 30 Days Of React'
 const title = 'Getting Started React'
 const subtitle = 'JavaScript Library'
 const author = {
-  firstName: 'Asabeneh',
-  lastName: 'Yetayeh',
+  firstName: '槐序',
+  lastName: '廿二',
 }
-const date = 'Oct 2, 2020'
+const date = 'Jan 4, 2023'
 
-// JSX element, header
+// jsx header
 const header = (
-  <header>
-    <div className='header-wrapper'>
-      <h1>{welcome}</h1>
-      <h2>{title}</h2>
-      <h3>{subtitle}</h3>
-      <p>
-        Instructor: {author.firstName} {author.lastName}
-      </p>
-      <small>Date: {date}</small>
+    <header>
+        <div className='header-wrapper'>
+            <h1>{welcome}</h1>
+            <h2>{title}</h2>
+            <h3>{subtitle}</h3>
+            <p>{author.firstName} {author.lastName}</p>
+            <small>{date}</small>
+        </div>
+    </header>
+)
+// jsx main data
+const techs = ['HTML','CSS','JavaScript']
+const techsFormatted = techs.map((tech)=><li key={tech}>{tech}</li>)
+// jsx main
+const infoData ={
+    name:'槐序廿二',
+    age:27,
+    country:'china',
+    title:'web engineer',
+    gender:'女',
+    email:'**********@emial.com',
+    phone:'100-0000-0000',
+}
+const info =(
+    <div className='info-style'>
+        <h2>{infoData.name}</h2>    
+        <table>
+            <thead>
+                <tr>
+                    <th>age</th>    
+                    <th>country</th>  
+                    <th>title</th>    
+                    <th>gender</th>   
+                    <th>email</th>   
+                    <th>phone</th>   
+                </tr>    
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{infoData.age}</td>
+                    <td>{infoData.country}</td>
+                    <td>{infoData.title}</td>
+                    <td>{infoData.gender}</td>
+                    <td>{infoData.email}</td>
+                    <td>{infoData.phone}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
-  </header>
 )
-
-const numOne = 3
-const numTwo = 2
-
-const result = (
-  <p>
-    {numOne} + {numTwo} = {numOne + numTwo}
-  </p>
+// jsx img level2
+const imgList = [htmlImg,cssImg,reactImg]
+const img = imgList.map((url)=><img key={url} src={url}/>)
+// jsx level3
+const level3 =(
+    <div className='level3-wrapper'>
+        <h1>SUBSCRIBE</h1>
+        <p>Sign up with your email address to recelve news and updates</p>
+        <div className='input-wrapper'>
+        <input placeholder='First name'></input>
+        <input placeholder='Last name'></input>
+        <input placeholder='Email name'></input>
+        </div>
+        <button>Subscribe</button>
+    </div>
 )
-
-const yearBorn = 1820
-const currentYear = new Date().getFullYear()
-const age = currentYear - yearBorn
-const personAge = (
-  <p>
-    {' '}
-    {author.firstName} {author.lastName} is {age} years old
-  </p>
+const authorData ={
+    img:useImg,
+    name:'槐序廿二',
+    level:'Promary development,China',
+    skills:['HTML','CSS','JS','VUE','REACT'],
+    time:'Jan 04, 2023'
+}
+const skill = authorData.skills.map((item)=><div key={item}>{item}</div>)
+const authorInfo =(
+    <div className='author-wrapper'>
+        <div className='img-wrapper'><img src={authorData.img}/></div>
+        <h5>{authorData.name}</h5>
+        <p>{authorData.level}</p>
+        <h5>SKILLS</h5>
+        <div className='slill-wrapper'>{skill}</div>
+        <p>{authorData.time}</p>
+    </div>
 )
-
-// JSX element, main
-const techs = ['HTML', 'CSS', 'JavaScript']
-const techsFormatted = techs.map((tech) => <li>{tech}</li>)
-
-const user = (
-  <div>
-    <img src={asabenehImage} alt='asabeneh image' />
-  </div>
-)
-
-// JSX element, main
+//jsx main
 const main = (
-  <main>
-    <div className='main-wrapper'>
-      <p>
-        Prerequisite to get started{' '}
-        <strong>
-          <em>react.js</em>
-        </strong>
-        :
-      </p>
-      <ul>{techsFormatted}</ul>
-      {result}
-      {personAge}
-      {user}
-    </div>
-  </main>
+    <main>
+        <div className='main-wrapper'>
+            <p>Prerequisite to get started react.js:</p>
+            <ul>{techsFormatted}</ul>
+            {info}
+            <div className='img-wrapper'>{img}</div>
+            {level3}
+            {authorInfo}
+      </div>
+    </main>
 )
 
-const copyRight = 'Copyright 2020'
-
-// JSX element, footer
+// jsx footer
 const footer = (
-  <footer>
-    <div className='footer-wrapper'>
-      <p>{copyRight}</p>
-    </div>
-  </footer>
+    <footer>
+        <div className='footer-wrapper'><p>Copyright 2023</p></div>
+    </footer>
 )
-
-// JSX element, app
+// jsx app
 const app = (
-  <div className='app'>
-    {header}
-    {main}
-    {footer}
-  </div>
+    <div className='app'>
+      {header}
+      {main}
+      {footer}
+    </div>
 )
+const rootEle = document.getElementById('root') 
 
-const rootElement = document.getElementById('root')
-// we render the JSX element using the ReactDOM package
-ReactDOM.render(app, rootElement)
+ReactDOM.render(app,rootEle)
